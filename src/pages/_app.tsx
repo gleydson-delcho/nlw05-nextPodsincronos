bceaddf86e07109ca60a48443467682a3caf29d0
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2021-04-21 15:01:52 -0300

    Adiciona o terceiro dia concluído da NLW-05, adiciona a rota dinamica para a página de um podcast específico. Adiciona as estilizações e as rotas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/global.scss';
-import Header from './components/Header';
-import Player from './components/Player';
+import Header from '../components/Header';
+import Player from '../components/Player';
 
 import styles from '../styles/app.module.scss';
 
